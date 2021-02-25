031c51379d8bc74c204b6fd82a7d8b5b5e8eb065
lifengqiang <3045033935@qq.com>
2021-02-25 16:00:37 +0800

    添加images文件夹 编辑README.md文件

--- app/src/main/java/com/lfq/picacg/util/NetTools.kt
+++ app/src/main/java/com/lfq/picacg/util/NetTools.kt
@@ -6,7 +6,7 @@ import com.lfq.picacg.MyApplication
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-const val serverurl: String = "http://10.0.2.2:8080/"
+const val serverurl: String = "http://192.168.0.108:8080/"
 const val head_source_url = serverurl + "image/source/head/"
 const val head_small_url = serverurl + "image/small/head/"
 
